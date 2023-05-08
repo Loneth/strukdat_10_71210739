3f8a16a375353b4fee74502199b0c8504b8bdd63
dae <odilio.ganesha@ti.ukdw.ac.id>
2023-05-08 15:54:29 +0700

    Update strukdat_10_71210739.py
    a

--- strukdat_10_71210739.py
+++ strukdat_10_71210739.py
@@ -11,29 +11,30 @@ def mergesort(data):
         j = 0
         k = 0
 
-        while i < len(right) and j < len(left):
-            if right[i] >= left[j] :
-              data[k] = right[i]
-              i += 1
+        while i < len(left) and j < len(right):
+            if left[i] <= right[j]:
+                data[k] = left[i]
+                i += 1
             else:
-                data[k] = left[j]
+                data[k] = right[j]
                 j += 1
             k += 1
 
-        while i < len(right):
-            data[k] = right[i]
+        while i < len(left):
+            data[k] = left[i]
             i += 1
             k += 1
 
-        while j < len(left):
-            data[k]=left[j]
+        while j < len(right):
+            data[k] = right[j]
             j += 1
             k += 1
         return data
 
-lst1 = [1,2,3,4,5,6,7,8,9,10,19,24,12,6,129,59,1,2000,3,56]
-lst2 = [20,21,22,23,24,25,26,27]
-lst3 = [30,29,31,33,19,20,31,21,59]
+
+lst1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 19, 24, 12, 6, 129, 59, 1, 2000, 3, 56]
+lst2 = [20, 21, 22, 23, 24, 25, 26, 27]
+lst3 = [30, 29, 31, 33, 19, 20, 31, 21, 59]
 print(mergesort(lst1))
 print(mergesort(lst2))
 print(mergesort(lst3))
